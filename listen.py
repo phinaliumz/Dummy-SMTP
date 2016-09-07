8739705489f73583bb0bb0bea5cc7bd8193a97e8
Sami Kallio <sami.kallio@aitiofinland.com>
2016-09-07 15:23:31 +0300

    Add recipient and sent time information to email filename

--- listen.py
+++ listen.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # Original script written by Stuart Colville: http://muffinresearch.co.uk/archives/2010/10/15/fake-smtp-server-with-python/
 """A noddy fake smtp server."""
@@ -5,6 +6,8 @@
 import smtpd
 import asyncore
 import time
+import os
+from datetime import datetime
 
 class FakeSMTPServer(smtpd.SMTPServer):
     """A Fake smtp server"""
@@ -14,8 +17,15 @@ class FakeSMTPServer(smtpd.SMTPServer):
         smtpd.SMTPServer.__init__(*args, **kwargs)
 
     def process_message(*args, **kwargs):
-        mail = open("mails/"+str(time.time())+".eml", "w")
-        print "New mail from " + args[2]
+        emailTime = str(datetime.now())
+        emailTo = str(args[3])
+        directory = os.getcwd()
+        mail = open(directory + "/avuksi/Dummy-SMTP/mails/" + emailTo + "-" + emailTime +  ".eml", "w")
+        print datetime.now()
+        print "New mail from " + args[2] 
+        MORE = args
+        print " to "
+        print args[3] 
         mail.write(args[4])
         mail.close
         pass
